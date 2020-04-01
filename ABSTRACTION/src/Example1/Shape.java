46741d4e4e02df0b0edcdca19eb1cc46b99e8db2
Asus <Asus@LAPTOP-O9UISQ3R>
2020-04-01 09:54:12 +0600

    Only Abstraction No Interface

---
+++ ABSTRACTION/src/Example1/Shape.java
@@ -0,0 +1,23 @@
+package Example1;
+
+abstract public class Shape 
+
+{
+    
+	public String color;
+
+	public Shape(String color) 
+	
+	{
+		super();
+		this.color = color;
+	}
+	
+	
+	
+    abstract public double area (); // Abstracted method
+   
+    
+	
+	
+}
